9bdf8a35719f4506e4c71642f5c7fbc2e2fcc863
boychina <boychina@outlook.com>
2017-08-18 18:35:58 +0800

    增加ng-zorro组件，搭建基础框架

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 import { AppComponent } from './app.component';
 
+import { NzDemoButtonTypeComponent } from './app.child';
+import { NzDemoTableSelectionAndOperationComponent } from './app.table';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NzDemoButtonTypeComponent,
+        NzDemoTableSelectionAndOperationComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        BrowserAnimationsModule,
+        NgZorroAntdModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
+
