f7e17f7b1ec4de9eb32e5b94ce99328f13743c3d
boychina <boychina@outlook.com>
2017-09-13 13:53:08 +0800

    完善layout架构

---
+++ src/app/Layout/layout.module.ts
@@ -0,0 +1,5 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
+
