1cb14ffe95a1dc9790feb6f558f9f7ba7505acef
boychina <boychina@outlook.com>
2017-09-14 17:21:22 +0800

    完成home页框架搭建，tab切换完成

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -21,4 +21,43 @@ export class HomeComponent implements OnInit {
     onLoggedin() {
         localStorage.setItem('isLoggedin', 'true');
     }
+
+     tabs = [
+        {
+            name: 'Tab 1',
+            index: 1
+        },
+        {
+            name: 'Tab 2',
+            index: 2
+        },
+        {
+            name: 'Tab 3',
+            index: 3
+        },
+        {
+            name: 'Tab 4',
+            index: 4
+        },
+        {
+            name: 'Tab 5',
+            index: 5
+        },
+        {
+            name: 'Tab 6',
+            index: 6
+        }
+    ];
+
+    closeTab(tab) {
+        this.tabs.splice(this.tabs.indexOf(tab), 1);
+    };
+
+    newTab() {
+        let i = 10;
+        this.tabs.push({
+            name: 'New Tab',
+            index: ++i
+        });
+    };
 }
