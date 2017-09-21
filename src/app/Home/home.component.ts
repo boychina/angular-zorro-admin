4c89d1d7b9a067e4d074a602fd269e91edabde50
boychina <boychina@outlook.com>
2017-09-21 14:34:57 +0800

    从返回菜单数据中设置默认卡片显示

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -17,9 +17,11 @@ export class HomeComponent implements OnInit {
 
     url: string = 'http://127.0.0.1:3000/HomeRoute/getMenusData';
     menus = [];
-    selectedIndex: number = 2;
+    selectedIndex: number = 0;
     menuItemId: string = "";
     childItemId: string = "";
+    tabs = [];
+
     ngOnInit() {
         let me = this;
         let body = JSON.stringify({
@@ -31,20 +33,34 @@ export class HomeComponent implements OnInit {
             //将获取到的菜单数据赋值为菜单数据
             console.log(res.json());
             me.menus = res.json();
+            me.setDefaultTab(me.menus);
         });
-    }
-
-    
-    onClick(parm) {
-        
     };
 
-    tabs = [
-        {
-            name: "6601",
-            index: "6601"
+    // ngDoCheck(value) {
+    //     // console.log("value", value);
+    // };
+
+    setDefaultTab(menus) {
+        let me = this,
+            parm;
+        if(menus.length !== 0){
+            parm = me.menus[0];
+            for(let i=0; i < 3; i++){
+                if(parm.children && parm.children.length !== 0) {
+                    parm = parm.children[0];
+                }
+            }
+        } else {
+            return ;
         }
-    ];
+        console.log(parm);
+        me.tabs.push({
+            name: parm.id,
+            index: parm.id
+        });
+        console.log("me.tabs",me.tabs);
+    };
     
     closeTab(tab) {
         this.tabs.splice(this.tabs.indexOf(tab), 1);
@@ -96,9 +112,6 @@ export class HomeComponent implements OnInit {
         }
     };
     
-    ngOnChanges() {
-
-    };
 
     ngAfterContentChecked() {
 
