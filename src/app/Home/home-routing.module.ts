7e8d1139d7f1e447ad1a0b1f0c9e72e77bee3f36
boychina <boychina@outlook.com>
2017-09-25 18:29:45 +0800

    删除多余注释

--- src/app/Home/home-routing.module.ts
+++ src/app/Home/home-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
-// import { IndexPageComponent } from '../IndexPage/indexPage.component';
 
 const routes: Routes = [
     {
         path: '',
         component: HomeComponent
-    },
-    // {
-    //     path: 'indexpage',
-    //     component: IndexPageComponent
-    // }
+    }
 ];
 
 @NgModule({
