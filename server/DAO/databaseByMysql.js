52f4720b01cceb53648b5601aa357e61bf45cd1a
wb-zh358561 <wb-zh358561@antfin.com>
2018-02-01 19:34:44 +0800

    修改databaseByMysql

--- server/DAO/databaseByMysql.js
+++ server/DAO/databaseByMysql.js
@@ -1,3 +1,4 @@
+'use strict';
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
 	host     : 'localhost',
@@ -19,16 +20,29 @@ var  addSql = 'INSERT INTO websites(Id,name,url,alexa,country) VALUES(0,?,?,?,?)
 var  addSqlParams = ['菜鸟工具', 'https://c.runoob.com','23453', 'CN'];
 //增
 connection.query(addSql,addSqlParams,function (err, result) {
-        if(err){
-         console.log('[INSERT ERROR] - ',err.message);
-         return;
-        }        
- 
-       console.log('--------------------------INSERT----------------------------');
-       //console.log('INSERT ID:',result.insertId);        
-       console.log('INSERT ID:',result);        
-       console.log('-----------------------------------------------------------------\n\n');  
+    if(err){
+        console.log('[INSERT ERROR] - ',err.message);
+        return;
+    }        
+
+    console.log('--------------------------INSERT----------------------------');
+    //console.log('INSERT ID:',result.insertId);        
+    console.log('INSERT ID:',result);        
+    console.log('-----------------------------------------------------------------\n\n');  
 });
+var dbCtrl = function(col, ctrl){
+    return function(){
+        // 将arguments转换为数组
+        var arg = [].slice.apply(arguments);
+        connection.query(addSql, addSqlParams, function(err, result){
+            if(err){
+                console.log('数据库链接出错：' + err.message);
+            } eles {
+                
+            }
+        })
+    }
+}
 
 
 connection.end();
